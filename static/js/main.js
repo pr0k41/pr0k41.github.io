5abe97028722c31d1c517e129954ba53dd030fe3
Prokai <114640991+pr0k41@users.noreply.github.com>
2024-12-27 18:31:02 +0530

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -10873,7 +10873,7 @@
                         children: [
                             (0, ft.jsx)(At, { src: n }),
                             (0, ft.jsx)(Pt, {
-                                placeholder: "Search...",
+                                placeholder: "Search",
                                 type: "input",
                                 onKeyUp: function (n) {
                                     return (
